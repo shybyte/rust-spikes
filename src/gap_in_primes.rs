e2c04731e93e5dadfffad485114870ed6671aae4
shybyte <ein@volloeko.de>
2017-03-09 09:47:40 +0100

    made gap in primes faster

--- src/gap_in_primes.rs
+++ src/gap_in_primes.rs
@@ -1,22 +1,28 @@
-fn gap(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
+fn gap_slow(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
     let primes = (m..n + 1).filter(|&x| is_prime(x)).collect::<Vec<_>>();
     primes.windows(2)
         .find(|prim| prim[1] - prim[0] == g as u64)
         .and_then(|pair| Some((pair[0], pair[1])))
 }
 
+fn gap(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
+    let mut primes = (m..n + 1).filter(|&x| is_prime(x));
+    primes.next().and_then(|mut prev| {
+        primes.find(|&prim|
+            if prim - prev == g as u64 {
+                true
+            } else {
+                prev = prim;
+                false
+            }
+        ).and_then(|second| Some((prev, second)))
+    })
+}
+
+// assumption x > 2
 fn is_prime(x: u64) -> bool {
-    if x == 1 {
-        return false;
-    }
-    let mut t = 2;
-    while t * t <= x {
-        if x % t == 0 {
-            return false;
-        }
-        t += 1;
-    }
-    return true;
+    let sqrt_x = (x as f64).sqrt() as u64;
+    (2..sqrt_x + 1).all(|t| x % t != 0)
 }
 
 
@@ -26,8 +32,6 @@ fn testing(g: i32, m: u64, n: u64, exp: Option<(u64, u64)>) -> () {
 
 #[test]
 fn test_is_prime() {
-    assert_eq!(is_prime(1), false);
-    assert_eq!(is_prime(2), true);
     assert_eq!(is_prime(3), true);
     assert_eq!(is_prime(4), false);
     assert_eq!(is_prime(5), true);
